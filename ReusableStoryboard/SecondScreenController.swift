0902cab1ecb918e25f577e06dd192d0d6bfbb5cd
Ryan Grey <rgrey@scottlogic.co.uk>
2018-11-06 15:34:10 +0000

    Use reusable storyboard in two controllers
    Add ability to navigate between controllers

---
+++ ReusableStoryboard/SecondScreenController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondScreenController.swift
+//  ReusableStoryboard
+//
+//  Created by Ryan Grey on 06/11/2018.
+//  Copyright © 2018 scottlogic. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreenController: UIViewController {
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
